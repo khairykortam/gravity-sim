744d6450ce8f8c083757c06ab5fdab35322686b3
khairykortam <khairykortam270@gmail.com>
2026-09-17 00:53:36 +0300

    added basic background setup

---
+++ idx.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,canvas.width,canvas.height);
